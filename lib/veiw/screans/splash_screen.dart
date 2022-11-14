cb322729cade7553cde445f07a47bb12fc366ed2
ahmednabil <ahmednabil65028467@gmail.com>
2022-11-14 15:07:26 +0200

    ahmed nabil 3

--- lib/veiw/screans/splash_screen.dart
+++ lib/veiw/screans/splash_screen.dart
@@ -12,35 +12,33 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              Expanded(
-                child: AnimatedSplashScreen(
-                  splash: Image.asset(
-                    'assets/images/app_logo.png',
-                    width: 250.w,
-                    fit: BoxFit.contain,
-                  ),
-                  centered: true,
-                  curve: Curves.easeInCirc,
-                  splashTransition: SplashTransition.fadeTransition,
-                  animationDuration: const Duration(milliseconds: 1000),
-                  splashIconSize: 1000.w,
-                  backgroundColor: backgroundColor,
-                  nextScreen: const HomePage(),
+        child: Column(
+          children: [
+            Expanded(
+              child: AnimatedSplashScreen(
+                splash: Image.asset(
+                  'assets/images/app_logo_img.png',
+                  width: 250.w,
+                  fit: BoxFit.contain,
                 ),
+                centered: true,
+                curve: Curves.easeInCirc,
+                splashTransition: SplashTransition.fadeTransition,
+                animationDuration: const Duration(milliseconds: 1000),
+                splashIconSize: 1000.w,
+                backgroundColor: backgroundColor,
+                nextScreen: const HomePage(),
               ),
-              Padding(
-                padding: EdgeInsets.all(10.w),
-                child: AppText(
-                  txt: 'v.10.2.3.0',
-                  color: grayColor,
-                  size: 15.sp,
-                ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.w),
+              child: AppText(
+                txt: 'v.10.2.3.0',
+                color: grayColor,
+                size: 15.sp,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
